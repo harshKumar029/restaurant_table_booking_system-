bbfdf3d8a29a8110c0b4913fd90d714ef27e6ff6
harshKumar029 <harshanand029@gmail.com>
2025-01-05 02:10:13 +0530

    Suspense

--- Frontend/src/app/layout.js
+++ Frontend/src/app/layout.js
@@ -1,13 +1,16 @@
 // import Footer from '@/components/Footer';
 import Navbar from '../components/Navbar';
 import './globals.css'; // Import global styles
+import { Suspense } from 'react';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+      <Suspense fallback={<div>Loading...</div>}>
         <Navbar />
         {children}  
+        </Suspense>
       </body>
     </html>
   );
